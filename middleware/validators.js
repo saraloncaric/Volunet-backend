dad6f6b55a8d39e1f3cb7f05c8348397111d9582
saraloncaric <167783165+saraloncaric@users.noreply.github.com>
2026-07-18 15:00:42 +0200

    popravljeno za registraciju

--- middleware/validators.js
+++ middleware/validators.js
@@ -8,26 +8,19 @@ export const handleValidationErrors = (req, res, next) => {
     next()
 }
 export const validatorRegistracija = [
-    body('ime')
-        .trim()
-        .notEmpty().withMessage('Ime je obavezno')
-        .isLength({ min: 2 }).withMessage('Ime mora imati minimalno 2 znaka')
-        .escape(),
-    body('prezime')
-        .trim()
-        .notEmpty().withMessage('Prezime je obavezno')
-        .isLength({ min: 2 }).withMessage('Prezime mora imati minimalno 2 znaka')
-        .escape(),
     body('email')
         .trim()
-        .notEmpty().withMessage('Email je obavezno')
+        .notEmpty().withMessage('Email je obavezan')
         .isEmail().withMessage('Email adresa nije ispravna'),
+
     body('password')
         .notEmpty().withMessage('Lozinka je obavezna')
         .isLength({ min: 6 }).withMessage('Lozinka mora imati minimalno 6 znakova'),
+
     body('role')
         .notEmpty().withMessage('Obavezno polje')
-        .isIn(['volonter', 'udruga']).withMessage('Rola mora biti volonter ili udruga'),
+        .isIn(['volonter', 'udruga'])
+        .withMessage('Rola mora biti volonter ili udruga'),
     handleValidationErrors
 ]
 export const validatorLogin = [
